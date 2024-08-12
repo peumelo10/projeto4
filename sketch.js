568ce176335879962454e8da9d16bbec454ca5c3
peumelo10 <00001086595622sp@al.educacao.sp.gov.br>
2024-08-12 20:12:18 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let imagem;
+let musica;
+
+function setup() {
+  createCanvas(1000, 800);
+  palavra = empada();
+  musica.loop();
+}
+function preload(){
+  imagem = loadImage("neymar.jpeg");
+  musica = loadSound("musica.mp3");
+}
+function draw() {
+  background("violet");
+  image(imagem,0,0,1000,800);
+  batata();
+  coxinha();
+  empada();
+}
+
+function batata(){
+  let minimo = 0;
+  let maximo = width;
+  let seguir = map(mouseX, 0, width, 1, palavra.length);
+  let inicio = palavra.substring(0,seguir);
+  text(inicio,500,400);
+}
+
+function coxinha(){
+  fill("lightblue");
+  textSize(40);
+  textAlign(CENTER,CENTER);  
+}
+
+function empada(){
+  let palavras = ["volta" , "neymar", "santos", "te", "espera<3"];
+  return random(palavras);
+}
